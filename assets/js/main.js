d801464bb94e3048a32e59b3b0a7f7861820c25e
lorenzotaddei <lorenzo.taddei@outlook.it>
2023-01-24 10:57:33 +0100

    fixing click issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,28 +116,6 @@
 
 })(jQuery);
 
-<<<<<<< HEAD
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-=======
 function showModal(event, modalId, imgSrc) {
     event.preventDefault();
 
@@ -162,4 +140,3 @@ window.onclick = function(event) {
 	  modal.style.display = "none";
 	}
   }
->>>>>>> 50a73daccc6c36e471b46918b883e6849c34f066
